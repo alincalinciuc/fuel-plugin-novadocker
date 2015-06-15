a38d44f7230f71b63e0e7845dfc5d12bb7acbd50
Daniel Depaoli <ddepaoli@create-net.org>
2015-06-15 17:07:46 +0200

    Add controller bash script

---
+++ deployment_scripts/controller.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+##Variables
+LOG_FILE="/var/log/docker-plugin"
+
+#Edit and restart glance
+sed -i -e s/.*"container_formats".*/"container_formats=ami,ari,aki,bare,ovf,ova,docker"/ /etc/glance/glance-api.conf
+service glance-api restart
+
+curl -sSL https://get.docker.com/ubuntu/ | sudo sh
+
+#source /root/openrc admin admin
+#docker pull cirros
+#docker save cirros | glance image-create --is-public=True --container-format=docker --disk-format=raw --name cirros
