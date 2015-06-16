3f1f5a270724924fae4cfb10c4644197e917425a
Daniel Depaoli <ddepaoli@create-net.org>
2015-06-16 10:44:13 +0200

    Add log in controller.sh

--- deployment_scripts/controller.sh
+++ deployment_scripts/controller.sh
@@ -4,9 +4,14 @@
 LOG_FILE="/var/log/docker-plugin"
 
 #Edit and restart glance
+echo "Change glance-api.conf config file" >> $LOG_FILE
 sed -i -e s/.*"container_formats".*/"container_formats=ami,ari,aki,bare,ovf,ova,docker"/ /etc/glance/glance-api.conf
-service glance-api restart
 
-curl -sSL https://get.docker.com/ubuntu/ | sudo sh
+echo "Restart glance api. PID: `service glance-api status`" >> $LOG_FILE
+service glance-api restart
+echo "Glance api restarted. PID: `service glance-api status`" >> $LOG_FILE
+
+echo "Install Docker" >> $LOG_FILE
+curl -sSL https://get.docker.com/ubuntu/ | sh
 
 exit 0
