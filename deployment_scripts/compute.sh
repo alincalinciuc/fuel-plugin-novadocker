7db757a0a5e84496bdf5a4bc00c4bb130c210302
Daniel Depaoli <ddepaoli@create-net.org>
2015-06-17 10:59:20 +0200

    Save docker images in compute node after installing docker

--- deployment_scripts/compute.sh
+++ deployment_scripts/compute.sh
@@ -23,7 +23,7 @@ all_nodes=`hiera fuel-plugin-docker | sed -e 's/=>/:/g' | \
 python -c \
 'import json; \
 import sys; \
-print str(json.load(sys.stdin)["fuel-plugin-docker-2_checkbox"]).lower()'`
+print str(json.load(sys.stdin)["fuel-plugin-docker_checkbox"]).lower()'`
 
 #Check if the name of the node contains 'docker'
 if (hiera user_node_name|grep -i docker) || $all_nodes
@@ -39,10 +39,13 @@ fi
 curl -sSL https://get.docker.com/ubuntu/ | sh
 
 #Install nova-docker
+#For more details please refer to https://wiki.openstack.org/wiki/Docker
 apt-get update
 apt-get install git
+#Install pip from this script to avoid possibile problem with the version in repository
 wget -O- https://raw.github.com/pypa/pip/master/contrib/get-pip.py | python
 ln -s `which pip` /usr/bin/pip
+#Use a version of nova-docker adapt for fuel 6.1 Juno.
 git clone -b stable/juno https://github.com/ddepaoli3/nova-docker.git
 cd nova-docker
 python setup.py install
@@ -54,5 +57,20 @@ echo 'ln: CommandFilter, /bin/ln, root' >> /etc/nova/rootwrap.d/docker.filters
 usermod -G docker nova
 
 service nova-compute restart
+
+#Save images in compute node.
+#For a known limit the compute search the image locally
+#and don't get it from glance
+images=(`hiera fuel-plugin-docker | \
+sed -e 's/=>/:/g' | \
+python -c \
+'import json; \
+import sys; \
+print json.load(sys.stdin)["fuel-plugin-docker_text"].replace(" ","").replace(","," ")'`)
+
+for image in ${images[@]}
+do
+    docker pull $image
+done
 
 exit 0
