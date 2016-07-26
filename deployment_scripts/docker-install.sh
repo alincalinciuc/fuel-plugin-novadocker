0910e5dddb670ac3bb7cb885ce3b8228eaf47ca6
hiago-miguel <miguel.hiago@gmail.com>
2016-07-26 09:30:49 -0300

    Add recommended package to docker installation

--- deployment_scripts/docker-install.sh
+++ deployment_scripts/docker-install.sh
@@ -19,7 +19,7 @@ docker_install_ubuntu_trusty()
     apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
     echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list
     apt-get update
-    apt-get install --yes docker-engine
+    apt-get install --yes linux-image-extra-$(uname -r) docker-engine
 }
 
 docker_install_centos_6()
